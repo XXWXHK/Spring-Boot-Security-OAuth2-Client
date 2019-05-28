79938d95c476aa451d970cffa99a0c06243eaaaa
徐晓伟 <xuxiaowei@live.hk>
2019-05-28 21:12:27 +0800

    Spring Security 安全控制 配置

---
+++ src/main/java/cn/com/xuxiaowei/securityoauth2client/config/security/WebSecurityConfigurerAdapterConfig.java
@@ -0,0 +1,32 @@
+package cn.com.xuxiaowei.securityoauth2client.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Security 安全控制 配置
+ *
+ * @author xuxiaowei
+ */
+@Configuration
+public class WebSecurityConfigurerAdapterConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+}
